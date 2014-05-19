8534db0c4de11332cdcb20f30c5faf367f9f0ab0
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-05-19 16:17:03 +0100

    added swedish and updated strings

--- pub/js/lang/dummy.php
+++ pub/js/lang/dummy.php
@@ -2,6 +2,7 @@
 /**
  * Here for source code extraction until xgettext is applied to js files too
  */
+ 
 _x('OK','Message label');
 _x('Warning','Message label');
 _x('Error','Message label');
@@ -15,6 +16,19 @@ __('Already up to date with source code');
 _n('1 new string added','%s new strings added', 2 );
 _n('1 obsolete string removed','%s obsolete strings removed', 2 );
 __('Your changes will be lost if you continue without saving');
+
+
+/**
+ * These items aren't ready for translation in source code yet because they come from Loco core, but they will be eventually
+ */
+// label for the window pane holding native translation 
+_x('Source text','Editor pane');
+// where %s is the language, e.g French translation
+_x('%s translation','Editor pane'); 
+// label for the window pane holding translator comments
+_x('Comments','Editor pane');
+// label for the window pane holding message context
+_x('Context','Editor pane');
 
 
 /**
