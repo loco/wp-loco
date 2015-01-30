bd1a6398fedcf196843dc4bb8c3dd7ec680a074f
Tim Whitlock <r-bitbucket@timwhitlock.info>
2015-01-30 18:17:30 +0000

    library build

--- pub/js/lang/loco-translate-nl_NL.js
+++ pub/js/lang/loco-translate-nl_NL.js
@@ -3,7 +3,7 @@
  * Project: loco.po conversion
  * Release: Working copy
  * Locale: nl_NL, Dutch
- * Exported at: Wed, 21 Jan 2015 14:04:40 +0000 
+ * Exported at: Fri, 30 Jan 2015 17:55:13 +0000 
  */
 loco = window.loco||{}, loco.t = function( pairs ){
     
