987b75b2e9d191b398ccd28dbd8a512f13e77743
Tim Whitlock <r-bitbucket@timwhitlock.info>
2018-05-17 11:58:10 +0100

    minor library update, php 7 fix and removal of redunant methods

--- src/compat/JsonSerializable.php
+++ src/compat/JsonSerializable.php
@@ -4,6 +4,7 @@
 /**
  * Placeholder for missing interface in PHP < 5.4.
  * Can't be invoked automatically, so always do: json_encode( $obj->jsonSerialize() )
+ * Note that this shim is also present in WordPress >= 4.4.0
  */
 if( ! interface_exists('JsonSerializable') ){
     interface JsonSerializable {
@@ -15,6 +16,7 @@ if( ! interface_exists('JsonSerializable') ){
 
 /**
  * Redundant interface so this file will autoload when JsonSerializable is referenced
+ * @internal
  */
 interface Loco_compat_JsonSerializable extends JsonSerializable {
     
